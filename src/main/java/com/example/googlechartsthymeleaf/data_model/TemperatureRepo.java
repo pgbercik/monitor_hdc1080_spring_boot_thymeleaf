277429593aa72ebf089dc2f0e415c9400c5023de
Piotr <pgbercik@gmail.com>
2021-05-04 22:47:56 +0200

    refactor

--- src/main/java/com/example/googlechartsthymeleaf/data_model/TemperatureRepo.java
+++ src/main/java/com/example/googlechartsthymeleaf/data_model/TemperatureRepo.java
@@ -9,16 +9,18 @@ import java.util.List;
 @Repository
 public interface TemperatureRepo extends CrudRepository<Temperature,Long> {
     //    do tabelki
+    /** ostatnie 60 próbek*/
     public List<Temperature> findTop60ByOrderByIdDesc();
+
     public List<Temperature> findAll();
 
     //do wykresu
 
-    /*24h*/
+    /**ostatnie 24h co 5 minut*/
     @Query("SELECT t FROM Temperature t WHERE t.id >=((Select max(t.id) from Temperature t) -1440) AND MOD (t.id,2.5)=0 order by t.id")
     public List<Temperature> findLast24h();
 
-    /*kilka h*/
+    /** ostatnie 6 h co minutę*/
     @Query("SELECT t FROM Temperature t WHERE t.id >=((Select max(t.id) from Temperature t) -360) order by t.id")
     public List<Temperature> findTop360();
 
